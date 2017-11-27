147f098840ca63e19c0778d9139248c16bd54d67
ryvnf <ryvnf@users.noreply.github.com>
2017-11-27 18:34:24 +0100

    fixed bug when map switches during cmdmode

--- code/client/cl_console.c
+++ code/client/cl_console.c
@@ -855,9 +855,9 @@ void Con_Close( void ) {
 	if ( !com_cl_running->integer ) {
 		return;
 	}
-	Field_Clear( &g_consoleField );
 	Con_ClearNotify ();
 	Key_SetCatcher( Key_GetCatcher( ) & ~KEYCATCH_CONSOLE );
+	cmdmode = qfalse;
 	con.finalFrac = 0;				// none visible
 	con.displayFrac = 0;
 }
